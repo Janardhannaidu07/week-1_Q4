84b4f4a97e6abdbc18b6bf99be6fce6d718d4b74
JANARDHAN NAIDU M <24331a05h7@mvgrce.edu.in>
2025-12-15 19:56:02 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,6 @@
+import math
+print("Value of pi:", math.pi)
+print("Square root of 25 :", math.sqrt(25))
+print("Sin 90 degrees : ", math.sin(math.radians(90)))
+print("Cos 0 degrees :", math.cos(math.radians(0)))
+
